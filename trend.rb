090a0490b6d3705e2eba2f2a896c8423fb6a02f0
srydos <srydos@gmail.com>
2017-03-22 15:21:40 +0900

    delete (:url)symbols data

--- trend.rb
+++ trend.rb
@@ -24,11 +24,18 @@ client = Twitter::REST::Client.new(
 #トレンドを表示 日本2345896 
 trends_local_plane = client.local_trends(23424856)
 trends_hash = {}
+trend_data  = []
 trends_hash["Time"] = Time.now
-trends_hash["data"] = trends_local_plane.map{ |rec| rec.to_hash }
-trends_hash["data"].map {|item| p item }
-=begin
+#hash in array in jsonの入れ子になっている
+trends_local_plane.to_hash[:trends].each do | hash |
+  #:urlは:queryと中身が同じなので取り除く
+  hash.delete(:url)
+  trend_data << hash
+end
+trends_hash["data"] = trend_data
+#  p rec.delete_if{ | key, value | key == :url};}
+#trends_hash["data"] = trends_local_plane.map{ |rec| rec.to_hash }
+#trends_hash.map{|item| p item }
 open("./data_trend.yml","a+") do |e|
   YAML.dump(trends_hash,e)
 end
-=end
